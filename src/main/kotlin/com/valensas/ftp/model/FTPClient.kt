dd284f2dc93afdd0f3dd5ffa7a309d7523468101
nusretkizilaslan <nusret.kizilaslan@valensas.com>
2024-09-16 14:13:35 +0300

    created base class FTPClient and extended SFTPClient, FTPSClient from it. Renamed the function 'connect' to 'authAndConnect' and added the authentication and connection logic for SFTP, FTP, and FTPS.

---
+++ src/main/kotlin/com/valensas/ftp/model/FTPClient.kt
@@ -0,0 +1,10 @@
+package com.valensas.ftp.model
+
+import org.apache.commons.net.ftp.FTPClient
+
+open class FTPClient : FTPClient() {
+    open fun authAndConnect(connectionModel: ConnectionModel) {
+        this.connect(connectionModel.host, connectionModel.port)
+        this.login(connectionModel.username, connectionModel.password)
+    }
+}
