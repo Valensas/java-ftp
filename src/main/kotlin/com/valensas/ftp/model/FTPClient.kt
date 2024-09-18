5db6ca264e8b5a4d9729820e706a1f33ed9725cb
Nusret Ali Kızılaslan <nusret.kizilaslan@valensas.com>
2024-09-18 14:52:57 +0300

    set file type only for ftp client

--- src/main/kotlin/com/valensas/ftp/model/FTPClient.kt
+++ src/main/kotlin/com/valensas/ftp/model/FTPClient.kt
@@ -1,11 +1,14 @@
 package com.valensas.ftp.model
 
+import org.apache.commons.net.ftp.FTP
 import org.apache.commons.net.ftp.FTPClient
 
 open class FTPClient : FTPClient() {
     open fun authAndConnect(connectionModel: ConnectionModel) {
         this.connect(connectionModel.host, connectionModel.port)
+        this.enterLocalPassiveMode()
         this.login(connectionModel.username, connectionModel.password)
+        this.setFileType(FTP.BINARY_FILE_TYPE)
     }
 
     open fun listFilesInfo(path: String): Map<String, Long> {
