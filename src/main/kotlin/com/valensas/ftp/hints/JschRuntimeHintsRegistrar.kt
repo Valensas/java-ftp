1b01d65c89c322e67146ed26750dc17f40d721c3
Alp Tuna Dagdanas <alp.tuna.dagdanas@valensas.com>
2025-12-26 14:32:36 +0300

    convert string to list

--- src/main/kotlin/com/valensas/ftp/hints/JschRuntimeHintsRegistrar.kt
+++ src/main/kotlin/com/valensas/ftp/hints/JschRuntimeHintsRegistrar.kt
@@ -29,24 +29,38 @@ class JschRuntimeHintsRegistrar : RuntimeHintsRegistrar {
         }
 
         val jschClasses =
-            """
-            com.jcraft.jsch.JSch,com.jcraft.jsch.Session,com.jcraft.jsch.ChannelSftp,
-            com.jcraft.jsch.UserAuth,com.jcraft.jsch.UserAuthPassword,
-            com.jcraft.jsch.UserAuthPublicKey,com.jcraft.jsch.UserAuthKeyboardInteractive,
-            com.jcraft.jsch.UserAuthNone,com.jcraft.jsch.UserAuthGSSAPIWithMIC,
-            com.jcraft.jsch.jce.DH,com.jcraft.jsch.jce.Random,
-            com.jcraft.jsch.jce.AES128CBC,com.jcraft.jsch.jce.AES256CBC,com.jcraft.jsch.jce.AES128CTR,
-            com.jcraft.jsch.jce.TripleDESCBC,com.jcraft.jsch.jce.BlowfishCBC,
-            com.jcraft.jsch.jce.HMACSHA1,com.jcraft.jsch.jce.HMACSHA256,com.jcraft.jsch.jce.HMACMD5,
-            com.jcraft.jsch.jce.SHA1,com.jcraft.jsch.jce.SHA256,com.jcraft.jsch.jce.MD5,
-            com.jcraft.jsch.jce.SignatureRSA,com.jcraft.jsch.jce.SignatureDSA,
-            com.jcraft.jsch.jce.KeyPairGenRSA,com.jcraft.jsch.jce.KeyPairGenDSA,
-            com.jcraft.jsch.DHG1,com.jcraft.jsch.DHG14,com.jcraft.jsch.DHGEX,
-            com.jcraft.jsch.DHGEX256
-            """.trimIndent()
-                .split(",")
-                .map { it.trim() }
-                .filter { it.isNotEmpty() }
+            listOf(
+                "com.jcraft.jsch.JSch",
+                "com.jcraft.jsch.Session",
+                "com.jcraft.jsch.ChannelSftp",
+                "com.jcraft.jsch.UserAuth",
+                "com.jcraft.jsch.UserAuthPassword",
+                "com.jcraft.jsch.UserAuthPublicKey",
+                "com.jcraft.jsch.UserAuthKeyboardInteractive",
+                "com.jcraft.jsch.UserAuthNone",
+                "com.jcraft.jsch.UserAuthGSSAPIWithMIC",
+                "com.jcraft.jsch.jce.DH",
+                "com.jcraft.jsch.jce.Random",
+                "com.jcraft.jsch.jce.AES128CBC",
+                "com.jcraft.jsch.jce.AES256CBC",
+                "com.jcraft.jsch.jce.AES128CTR",
+                "com.jcraft.jsch.jce.TripleDESCBC",
+                "com.jcraft.jsch.jce.BlowfishCBC",
+                "com.jcraft.jsch.jce.HMACSHA1",
+                "com.jcraft.jsch.jce.HMACSHA256",
+                "com.jcraft.jsch.jce.HMACMD5",
+                "com.jcraft.jsch.jce.SHA1",
+                "com.jcraft.jsch.jce.SHA256",
+                "com.jcraft.jsch.jce.MD5",
+                "com.jcraft.jsch.jce.SignatureRSA",
+                "com.jcraft.jsch.jce.SignatureDSA",
+                "com.jcraft.jsch.jce.KeyPairGenRSA",
+                "com.jcraft.jsch.jce.KeyPairGenDSA",
+                "com.jcraft.jsch.DHG1",
+                "com.jcraft.jsch.DHG14",
+                "com.jcraft.jsch.DHGEX",
+                "com.jcraft.jsch.DHGEX256",
+            )
 
         jschClasses.forEach {
             reflection.registerTypeIfPresent(
